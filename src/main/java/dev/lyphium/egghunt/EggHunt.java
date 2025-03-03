307465edeb40160d04b00843ac91ef082a594585
Lyphion <reallyphion@gmail.com>
2025-03-03 20:17:22 +0100

    Initial commit

---
+++ src/main/java/dev/lyphium/egghunt/EggHunt.java
@@ -0,0 +1,18 @@
+package dev.lyphium.egghunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EggHunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
